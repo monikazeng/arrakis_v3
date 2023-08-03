dd4da981120a9a5b00020ff97ca4de2a59d078e5
unknown <abharg22@asu.edu>
2023-08-03 21:15:37 +0100

    added a function in securityrepos

--- java-api/src/main/java/com/db/grad/javaapi/repository/SecurityRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/SecurityRepository.java
@@ -1,10 +1,20 @@
 package com.db.grad.javaapi.repository;
 
+import com.db.grad.javaapi.model.Dog;
 import com.db.grad.javaapi.model.Security;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SecurityRepository extends JpaRepository<Security,Integer> {
+//    @Query(nativeQuery = true, value = "select * from Security where Status = active")
+//    List<Security> findActiveBonds;
+    @Query(nativeQuery = true, value ="select issuer from Security where ID= :id")
+    List<Security> findIssuerfromBondID(Integer id);
+
+
 
 }
