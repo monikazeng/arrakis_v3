2830cc230fbeaf2d46ac98755ae1f703145ede64
unknown <abharg22@asu.edu>
2023-08-04 20:09:34 +0100

    page3 of layout

--- react-app3/ui/src/components/page3.jsx
+++ react-app3/ui/src/components/page3.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import styles from './Page3.css'
 
-export default function page3() {
+const Page3 = () => {
   return (
-    <div>page3</div>
+    <div>
+      <div id="dash"> 
+        <h2>Dashboard</h2>
+        <table>
+          <tr>Trade Books</tr>
+          <tr>Bonds maturing in 5 days</tr>
+          <tr>Bonds matured in last 5 days</tr>
+        </table>
+
+      </div>
+      <table id='table'>
+         <thead>
+           <tr>
+             <th>ID</th>
+             <th>ISIN</th>
+             <th>CUSIP</th>
+             <th>Maturity_Date</th>
+             <th>ISSUER</th>
+             <th>COUPON</th>
+             <th>TYPE</th>
+             <th>Face_Value</th>
+             <th>CURRENCY</th>
+             <th>STATUS</th>
+           </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    </div>
   )
 }
+
+export default Page3
+ 
