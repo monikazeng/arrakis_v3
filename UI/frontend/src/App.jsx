479d3a13d5b37bfebeed2a862dfb41f5de66ee2e
lchapata <lchapata@trincoll.edu>
2023-08-08 09:04:56 +0100

    dashboard

--- UI/frontend/src/App.jsx
+++ UI/frontend/src/App.jsx
@@ -8,12 +8,18 @@ import Dashboard from './components/Dashboard';
 import Booklist from './components/Booklist';
 import ActiveBonds from './components/ActiveBonds';
 import Maturity5days from './components/Maturity5days';
+
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route exact path="/" element={<Login/>} />
-      <Route path="/dashboard" element={<ActiveBonds/>} />
+     <Routes>
+        <Route exact path="/" element={<Login/>} />
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/IssuerList" element={<IssuerList/>}/>
+        <Route path="/Booklist" element={<Booklist/>} />
+        <Route path="/ActiveBonds" element={<ActiveBonds/>}/>
+        <Route path="/Maturity5days" element={<Maturity5days/>}/>
+      {/* <Route path="/dashboard" element={<Maturity5days/>} /> */}
     </Routes>
   </Router>
   );
--- UI/frontend/src/components/Dashboard.jsx
+++ UI/frontend/src/components/Dashboard.jsx
@@ -1,12 +1,32 @@
+// src/components/Dashboard.js
 import React from 'react';
+import { Link } from 'react-router-dom';
+import IssuerList from './IssuerList';
+import Booklist from './Booklist';
+import ActiveBonds from './ActiveBonds';
+import Maturity5days from './Maturity5days';
 
 const Dashboard = () => {
   return (
     <div>
-      <h2>Welcome to the Dashboard!</h2>
-      {/* Your dashboard content goes here */}
+      <h2>Home Dashboard</h2>
+      <div>
+        <Link to="/IssuerList">
+          <button>IssuerList</button>
+        </Link>
+        <Link to="/Booklist">
+          <button>Booklist</button>
+        </Link>
+        <Link to="/ActiveBonds">
+          <button>ActiveBonds</button>
+        </Link>
+        <Link to="/Maturity5days">
+          <button>Maturity5days</button>
+        </Link>
+      </div>
     </div>
   );
 };
 
 export default Dashboard;
+
